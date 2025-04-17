dd106e747362445884993efcb9d72750b81c2028
BalintCoder <gbalint2000@gmail.com>
2025-04-17 18:02:48 +0200

    Question, Answer, Language modells are created

---
+++ PLanguageQuizGame/PLanguageQuizGame/Models/Language.cs
@@ -0,0 +1,10 @@
+namespace PLanguageQuizGame.Models;
+
+public class Language
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    
+    public string Name { get; set; }
+    
+    public ICollection<Question> Questions { get; set; }
+}
